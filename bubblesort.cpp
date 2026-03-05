ee6318da96e30e8a6acb768def29453fc988cecc
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-03-05 18:40:21 +0700

    Menampilkan array dan jumplah pass

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -60,3 +60,26 @@ void bubbleSortArray()
 
   } while (pass <= n -1); // step 5
 }
+
+void display()
+{
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for  (int j = 0; j < n; j++)
+    {
+        cout << arr[j]; // menampilkan array
+        if (j < n - 1)
+        {
+            cout << " --> ";
+        }
+    }
+    cout << endl;
+    cout << endl;
+
+    cout << "Jumlah pass = " << n - 1 << endl; // menampilkan jumlah dari pass
+    cout << endl;
+    cout << endl;
+}
