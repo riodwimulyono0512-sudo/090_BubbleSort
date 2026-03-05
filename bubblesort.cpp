dabca833375e78b216a3deb6bb4f09de7657b6d9
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-03-05 17:42:26 +0700

    Deklarasi variable array=20

---
+++ bubblesort.cpp
@@ -0,0 +1,7 @@
+// Add Library
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[20]; // Deklarasi variable global array a dengan ukuran 20
+int n;       // Deklarasi variabel global n untuk menyimpan banyaknya elemen
