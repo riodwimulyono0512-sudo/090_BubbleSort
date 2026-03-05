e70aa45bc1eab296a9568a64bacd169b57027eb0
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-03-05 18:01:03 +0700

    Step by step

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -32,3 +32,31 @@ void input ()
         cin >> arr [i];
     }
 }
+
+void bubbleSortArray()
+{
+  int pass = 1;
+
+  do
+  {
+    for (int j = 0; j <= n -  1 - pass; j++)
+    { // step 2
+        if (arr[j] > arr [j + 1])
+        {// step 3
+            int temp;
+            temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+        }
+    }
+     pass += 1; //step 4
+
+    cout << "\nPass" << pass -1 << ": "; // Number of pass
+    for (int k = 0; k < n; k++)
+    {
+        cout << arr[k] << " "; // Menampilkan data pada number of pass
+    }
+    cout << endl;
+
+  } while (pass <= n -1); // step 5
+}
